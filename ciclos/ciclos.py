b24d61d0257ed283ca3f6bdd91fe92de652996e5
Llovascript <122043788@upq.edu.mx>
2024-01-30 19:46:30 -0600

    Update ciclos.py

--- ciclos/ciclos.py
+++ ciclos/ciclos.py
@@ -1,8 +1,10 @@
 #1.- numeros impares
 
 num = int(input("ingresa numero: "))
+odd_numbers = []
 
-for No in range(1, num):
-    if No % 2!=0:
-        num += No
-print(','.join(1))
+for i in range(1, num + 1):
+    if i % 2 != 0:
+        odd_numbers.append(str(i))
+
+print(",".join(odd_numbers))
