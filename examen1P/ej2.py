41f199cf2af9d96e62cdd11a4342d6f9a7b269f9
Llovascript <122043788@upq.edu.mx>
2024-02-07 20:03:31 -0600

    ejercicio 2 examen terminado

--- examen1P/ej2.py
+++ examen1P/ej2.py
@@ -1,1 +1,7 @@
 #escribir un programa que visualice en pantalla los numeros multiplos de 10 complendidos entre 10 y 1000
+
+num = int(input("Desde qué número deseas los múltiplos hasta el mil: "))
+
+for i in range(num, 1001, 10):
+    print(i,end=", ")
+
