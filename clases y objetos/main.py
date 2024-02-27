c378c17a78ca9ee65d162166be6f626b70c2afed
Llovascript <157436170+Llovascript@users.noreply.github.com>
2024-02-26 20:35:00 -0600

    Ejemplo Encapsulamiento

--- clases y objetos/main.py
+++ clases y objetos/main.py
@@ -1,17 +1,46 @@
 from Personaje import *
 from armas import *
 
+
+#solicitar datos Spartan
+print("====Datos de Heroe====")
+nombreS=input("Escribe el nombre de tu Spartan: ")
+especieS=input("Escribe la especie de tu Spartan: ")
+alturaS=float(input("Escribe la altura de tu Spartan: "))
+
+#solicitar datos Nemesis
+print("====Datos de Villano====")
+nombreN=input("Escribe el nombre de tu Nemesis: ")
+especieN=input("Escribe la especie de tu Nemesis: ")
+alturaN=float(input("Escribe la altura de tu Nemesis: "))
+
 #creamos el obj de la clase personaje
-Spartan = Personaje()
+Spartan = Personaje(especieS,nombreS,alturaS)
+Nemesis = Personaje(especieN,nombreN,alturaN)
 arma=armas()
 
+print("====El obj Spartan contine====")
 print(Spartan.nombre)
 print(Spartan.especie)
 print(Spartan.altura)        
+print("")
+
+print("====El obj Villano contine====")
+print(Nemesis.nombre)
+print(Nemesis.especie)
+print(Nemesis.altura) 
+print("")
 
 #usamos los metodos del spartan
+print("====Metodos del obj Spartan====")
 Spartan.correr(True)
 Spartan.lanzarGranada()
+print("")
+
+print("====Metodos del obj Nemesis====")
+Nemesis.correr(True)
+Nemesis.lanzarGranada()
+print("")
 
 #usamos metodos del arma
 arma.seleccionarArma(Spartan.nombre)
--- clases y objetos/personaje.py
+++ clases y objetos/personaje.py
@@ -1,16 +1,19 @@
 class Personaje:
     
     #atributo de personaje
-    especie="Humano"
-    nombre="John"
-    altura= 2.18
+    #declaramos el constructor para crear los obj
+    def __init__(self, esp, nom, alt):
+        self.especie = esp
+        self.nombre = nom
+        self.altura = alt
+        
     
     #metodos del personaje
     def correr(self, estado):
         if (estado):
-            print("el personaje"+self.nombre+ "esta corriendo")
+            print("el personaje "+self.nombre+ "esta corriendo")
         else:
-            print("el personaje"+self.nombre+ " esta muerto")
+            print("el personaje "+self.nombre+ " esta muerto")
             
     def lanzarGranada(self):
         print(self.nombre+" pego una granada")
