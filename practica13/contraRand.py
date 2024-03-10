08e2bfb6e49de0782b50258674dc557d9ef300b5
Llovascript <157436170+Llovascript@users.noreply.github.com>
2024-03-09 22:31:11 -0600

    interfaz terminada

--- practica13/contraRand.py
+++ practica13/contraRand.py
@@ -3,8 +3,8 @@ import random
 
 class Contraseña:
 
-    def __init__(self, length=8, include_uppercase=False, include_special_chars=False):
-        self.length = length
+    def __init__(self, long=8, include_uppercase=False, include_special_chars=False):
+        self.long = long
         self.include_uppercase = include_uppercase
         self.include_special_chars = include_special_chars
 
@@ -15,14 +15,14 @@ class Contraseña:
         if self.include_special_chars:
             characters += string.punctuation
 
-        password = ''.join(random.choice(characters) for _ in range(self.length))
+        password = ''.join(random.choice(characters) for _ in range(self.long))
         return password
 
     def check_password_strength(self, password):
-        length = len(password)
-        if length in range(8,11):
+        long = len(password)
+        if long < 8:
             return "Débil"
-        elif length in range (12,15):
+        elif long in range (8,15):
             return "Medio"
         else:
             return "Fuerte"
--- practica13/interfaz.py
+++ practica13/interfaz.py
@@ -1,7 +1,7 @@
-from tkinter import Tk, messagebox, Label, Entry, BooleanVar, Checkbutton, Button
+from tkinter import Tk, messagebox, Label, Entry, BooleanVar, Checkbutton, Button, Frame
 from contraRand import Contraseña
 
-def generate_and_print_password():
+def genera_password():
     length_input = length_entry.get()
     if length_input.strip() == "":
         length = 8
@@ -13,48 +13,41 @@ def generate_and_print_password():
 
     contraseña = Contraseña(length, include_uppercase, include_special_chars)
     password = contraseña.generate_password()
-    generated_password_label.config(text="Contraseña: " + password)
-
-    strength = contraseña.check_password_strength(password)
-    strength_label.config(text="Fuerza: " + strength)
+    strength = contraseña.check_password_strength(password) 
+    Passw.delete(0, 'end')  
+    Passw.insert(0, password)   
+    messagebox.showinfo("Contraseña generada", f"La contraseña generada es: {password}\n\nFuerza de la contraseña: {strength}")
+    
 
 #ventana
 ventana = Tk()
 ventana.title("Generador de Contraseñas")
-ventana.geometry("400x200")
+ventana.geometry("350x400")
+ventana.configure(bg='#C0D9E8')
 
-#Widget
-length_label = Label(ventana, text="Longitud del password (enter para 8 por default):")
-length_label.grid(row=0, column=0, sticky="w")
+# Widget
+length_label = Label(ventana, text="Longitud del password (8 por defecto)")
+length_label.place(x=30, y=30, width=250, height=30)
+
 length_entry = Entry(ventana)
-length_entry.grid(row=0, column=1)
-
-
-def opciones():
-    print(messagebox.askokcancel(message="¿Incluir mayúsculas?"), variable=uppercase_var)
+length_entry.place(x=120, y=60, width=50, height=30)
 
 uppercase_var = BooleanVar()
 uppercase_checkbox = Checkbutton(ventana, text="¿Incluir mayúsculas?", variable=uppercase_var)
-uppercase_checkbox.grid(row=1, column=0, sticky="w")
+uppercase_checkbox.place(x=50, y=100)
 
 special_chars_var = BooleanVar()
 special_chars_checkbox = Checkbutton(ventana, text="¿Incluir caracteres especiales?", variable=special_chars_var)
-special_chars_checkbox.grid(row=2, column=0, sticky="w")
+special_chars_checkbox.place(x=50, y=130)
 
-#boton
-opc_button= Button(ventana, text='opciones', bg='#FFE933', command=opciones)
-opc_button.configure(height=2, width=10)
-opc_button.pack()
+PasswL= Label(ventana, text="Contraseña generada:")
+PasswL.place(x=50, y=160, width=200, height=30)
 
-generate_button = Button(ventana, text="Generar Contraseña", command=generate_and_print_password, bg='#0D5CCE', fg='#FFFFFF')
-generate_button.grid(row=3, column=0, columnspan=2)
+Passw = Entry(ventana)
+Passw.place(x=50, y=200, width=200, height=30)
 
-
-#contraseña y fortaleza
-generated_password_label = Label(ventana, text="")
-generated_password_label.grid(row=4, column=0, columnspan=2)
-
-strength_label = Label(ventana, text="")
-strength_label.grid(row=5, column=0, columnspan=2)
+# Boton
+Gbutton = Button(ventana, text="Generar Contraseña", command=genera_password, bg='#0D5CCE', fg='#FFFFFF')
+Gbutton.place(x=50, y=250, width=200, height=30)
 
 ventana.mainloop()
